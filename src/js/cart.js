ebf9673cf26fdadf921bfdb18916c6ea87f20a0e
farah-st <farah.perezcarrillo@gmail.com>
2023-05-10 17:14:50 -0600

    new branch

--- src/js/cart.js
+++ src/js/cart.js
@@ -12,6 +12,7 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
 }
 
 function cartItemTemplate(item) {
