9e9f55df4599d7b4a5a593e927903da81ca0b5ce
farah-st <farah.perezcarrillo@gmail.com>
2023-05-19 18:43:23 -0600

    alert homework

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,8 +1,25 @@
- async function alertMessage(){
-  const response = await fetch("alerts.json");
-  const data = await response.json();
-  console.log(data);
- }
+//  async function alertMessage(){
+//   const response = await fetch("alerts.json");
+//   const data = await response.json();
+//   console.log(data);
+//  }
  
- alertMessage();
+//  alertMessage();
+
+
+fetch("alerts.json")
+  .then(response => response.json())
+  .then(data => {
+    // Loop through the data to create an alert for each item
+    data.forEach(item => {
+      // Create a new alert element
+      const alert = document.createElement("div");
+      alert.classList.add("alert");
+      alert.textContent = item.message;
+      alert.style.backgroundColor = item.backgroundColor;
+      
+      // Add the alert to the page
+      document.querySelector(".productList").appendChild(alert);
+    });
+  });
 
