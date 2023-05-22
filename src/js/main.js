6029de2c1dadcda3a4682a4450e3a76b86c68e80
farah-st <farah.perezcarrillo@gmail.com>
2023-05-22 17:58:41 -0600

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,1 @@
-import { updateCartSuperscript, loadHeaderFooter } from "./utils.mjs";
-import productList from "./productList.mjs";
 
-productList(".product-list", "tents", 4);
-loadHeaderFooter();
-updateCartSuperscript();
---
+++ src/js/product-list.js
@@ -0,0 +1,6 @@
+import { updateCartSuperscript, loadHeaderFooter } from "./utils.mjs";
+import productList from "./productList.mjs";
+
+productList(".product-list", "tents", 4);
+loadHeaderFooter();
+updateCartSuperscript();
