9c06e0504aef621838ba543e65f373544009359f
farah-st <farah.perezcarrillo@gmail.com>
2023-05-25 18:20:17 -0600

    fixing images

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -5,14 +5,13 @@ function productCardTemplate(product){
     // creates a template to join into HTML document
     const renderProduct = `<li class="product-card">
         <a href="../product_pages/index.html?product=${product.Id}">
-        <img src="${product.Image}" alt="${product.NameWithoutBrand}">
+        <img src="${product.Images.PrimaryMedium}" alt="${product.NameWithoutBrand}">
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.Name}</h2>
         <p class="product-card__price">$${product.FinalPrice}</p></a>
     </li>`
     return renderProduct;
 }
-
 
 export default async function productList(selector, category){
 // Creates li elements with productCardTemplate as template
