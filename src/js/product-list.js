3182403d12b51af9c02a39b71028782754960b3e
farah-st <farah.perezcarrillo@gmail.com>
2023-06-04 14:39:37 -0600

    adding event listener

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -6,3 +6,9 @@ const orderById = getParam("name", "FinalPrice");
 
 loadHeaderFooter();
 productList(".product-list", categoryId, orderById);
+
+const sortSelect = document.getElementById("sort-select");
+sortSelect.addEventListener("change", function(event) {
+    const orderBy = event.target.value;
+    productList(".product-list", categoryId, orderBy);
+  });
