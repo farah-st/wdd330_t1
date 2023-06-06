198a183716c21add936dbb56ea34a907b72bb2eb
farah-st <farah.perezcarrillo@gmail.com>
2023-06-06 17:18:03 -0600

    sorted products working

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -2,13 +2,13 @@ import { loadHeaderFooter, getParam } from "./utils.mjs";
 import productList from "./productList.mjs";
 
 const categoryId = getParam("category");
-const orderById = getParam("name", "FinalPrice");
+// const orderBy = getParam("orderBy", "Name");
 
 loadHeaderFooter();
-productList(".product-list", categoryId, orderById);
+productList(".product-list", categoryId);
 
 const sortSelect = document.getElementById("sort-select");
 sortSelect.addEventListener("change", function(event) {
-    const orderBy = event.target.value;
-    productList(".product-list", categoryId, orderBy);
-  });
+  const orderBy = event.target.value;
+  productList(".product-list", categoryId, orderBy);
+});
