aa181fea9dc7a9829085643400fe79ea01ddf54d
farah-st <farah.perezcarrillo@gmail.com>
2023-06-14 18:27:51 -0600

    week09

--- src/js/login.js
+++ src/js/login.js
@@ -1,14 +1,13 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
-import { checkLogin } from "./auth.mjs"; 
-import { formDataToJSON } from "./checkoutProcess.mjs";
-import { loginRequest } from "./externalServices.mjs";
-checkLogin();
+import { login } from "./auth.mjs"; 
+
+
 loadHeaderFooter();
 
 const redirect = getParam("redirect");
 
-document.getElementById("login-button").addEventListener("click", (e) => {
-    const form = document.forms["login"];
-    const jsonForm = formDataToJSON(form);
-    loginRequest(jsonForm);
-})
+document.getElementById("login-button").addEventListener("click", () => {
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    login({email, password}, redirect);
+});
