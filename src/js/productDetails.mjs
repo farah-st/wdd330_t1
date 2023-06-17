421c388e51e1bfb7a8147281dd15e2869da06532
Taylor Christensen <kuleman47@byui.edu>
2023-06-17 07:43:12 -0400

    comments added to local storage

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -70,20 +70,22 @@ function productNotFoundTemplate(){
 
  function addComment(e) {
   e.preventDefault();
-  const comments = getLocalStorage("so-comment");
-  if ((getLocalStorage("so-comment") === null)) {
+  
+  if ((getLocalStorage("so-comment") == null)) {
     setLocalStorage("so-comment", []);
-    console.log(getLocalStorage("so-comment"))
   };
+  const comments = getLocalStorage("so-comment");
   var myForm = document.forms[0];
   var chk_status = myForm.checkValidity();
   if (chk_status) {
+
     const json = formDataToJSON(document.forms["comment"]);
     json.todays_date = new Date();
     json.id = document.querySelector('input').dataset.id;
     
-    console.log(setLocalStorage("so-comment"))
-    setLocalStorage("so-comment", [json]);
+    comments.push(json);
+    setLocalStorage("so-comment", comments);
+    window.location.reload();
   }
       
   else {
