779ed92ee983bdaf1341e20c347e380ec4930d9c
Taylor Christensen <kuleman47@byui.edu>
2023-07-03 02:45:38 -0400

    carousel needs work

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -73,6 +73,10 @@ export function productDetailsTemplate(product){
 
           <div class="extra-images">
           ${displayExtraImages(product.Images.ExtraImages)}
+  <a class="prev" onclick="${plusSlides(-1, 0)}">&#10094;</a>
+  <a class="next" onclick="${plusSlides(1, 0)}">&#10095;</a>
+
+ 
           </div>
 
           <div class="product-detail__add">
@@ -228,3 +232,27 @@ function addComment(e) {
       });
   }
 }
+
+
+let slideIndex = [0, 1];
+let slideId = ["product-carousel"];
+
+showSlides(1, 0);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  //console.log(x);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 2) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  console.log(x[no]);
+  //x.style.display = "block";  
+}
+
